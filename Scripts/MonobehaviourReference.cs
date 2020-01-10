09c3f4c1db50df29f99e2605fe9d2f1e2f6f78dd
Matthew Samun <19292426+MSamun@users.noreply.github.com>
2020-01-10 11:30:15 -0500

    Upload file
    - This file references Monobehaviour in Unity, and all other scripts inherit from this one script. I figured having only one file inherit from Monobehaviour may improve performance (not entirely sure though.)

---
+++ Scripts/MonobehaviourReference.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Polyreid
+{
+    public abstract class MonobehaviourReference : MonoBehaviour
+    {
+        /* STRICTLY USED SO ALL THE OTHER SCRIPTS CAN INHERIT FROM THIS SCRIPT! */
+    }
+}
